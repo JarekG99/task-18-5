6ff637ffb9d716d3d854c004fd4868982c42ec87
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-04-12 19:56:24 +0200

    Add files

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+var env =process.env.NODE_ENV || 'development';
+
+const path = require('path');
+
+const plugins = [new HtmlWebpackPlugin({
+   template: 'client/index.html',
+   filename: 'index.html',
+   inject: 'body'
+})];
+
+module.exports = {
+    entry: [
+            'react-hot-loader/patch',
+            './client/index.js'
+          ],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'app_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                 test: /\.css$/,
+                 use: [
+                     { loader: 'style-loader'},
+                     {
+                         loader: 'css-loader',
+                         options: {
+                             modules: true
+                         }
+                      }
+                    ]
+                  }
+              ]
+        },
+
+
+     devServer: {
+      proxy: {
+          '/socket.io': {
+              target: 'http://localhost:3000',
+              ws: true
+          }
+      }
+    }
+}
